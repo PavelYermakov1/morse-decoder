12cad8a1aed667de95713643cb9c7235a521d4b3
Pavel Yermakov <pavelak137@mail.ru>
2020-03-02 23:47:45 +0300

    feat: complited task

--- src/index.js
+++ src/index.js
@@ -35,10 +35,29 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    ' ': ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+    var result = '';
+        for(let j = 0; j < expr.length; j += 10) {
+          let substr = expr.substr(j, 10);
+          var line = '';
+          for(let i = 0; i < substr.length; i += 2) {
+            if(substr == '**********') {
+              line += ' ';
+              i += 10;
+            }else if(substr.slice(i,i + 2) == 00) {
+              line += '';
+            }else if(substr.slice(i,i + 2) == 10) {
+              line += '.';
+            }else {
+              line += '-';
+            }
+          }
+          result += MORSE_TABLE[line];    
+        }
+        return result;
 }
 
 module.exports = {
